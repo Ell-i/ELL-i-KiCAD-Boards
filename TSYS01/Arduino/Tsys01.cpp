485f4a4e79a9ef9822662aa41975524977fcf853
Jaakko Laine <apocalyt@gmail.com>
2016-02-29 21:16:58 +0200

    Add arduino source files

---
+++ TSYS01/Arduino/Tsys01.cpp
@@ -0,0 +1,250 @@
+#include "Tsys01.h"
+#include <Wire.h>
+#include <SPI.h>
+#define TSYS_DEBUG 0
+
+
+// Use this constructor only for creating global variables
+Tsys01::Tsys01() {
+}
+
+Tsys01::Tsys01(uint8_t _communicationProtocol, uint8_t _powerPin)
+{
+  this->powerPin = _powerPin;
+  #if TSYS_DEBUG
+    Serial.println("TSYS01: Setting communication protocol");
+  #endif  
+    this->communicationProtocol = _communicationProtocol;
+    #if TSYS_DEBUG
+    Serial.println("TSYS01: Resetting sensor");
+  #endif
+    this->reset();
+  #if TSYS_DEBUG
+    Serial.println("TSYS01: Starting to read calibration");
+  #endif
+  delay(4);
+    this->readCalibration();
+  #if TSYS_DEBUG
+    Serial.println("TSYS01: Creating library object done");
+  #endif
+}
+
+Tsys01::Tsys01(uint8_t _communicationProtocol, uint8_t _powerPin, uint8_t _slaveSelect)
+{
+  this->powerPin = _powerPin;
+  #if TSYS_DEBUG
+    Serial.println("TSYS01: Setting communication protocol");
+  #endif
+  this->slaveSelectPin = _slaveSelect;
+  this->communicationProtocol = _communicationProtocol;
+  #if TSYS_DEBUG
+    Serial.println("TSYS01: Resetting sensor");
+  #endif
+  this->reset();
+  #if TSYS_DEBUG
+    Serial.println("TSYS01: Starting to read calibration");
+  #endif
+  delay(4);
+  this->readCalibration();
+}
+
+
+Tsys01::Tsys01(uint8_t _communicationProtocol, uint8_t _powerPin, uint8_t _slaveSelect, uint8_t _protocolPin)
+{
+  this->powerPin = _powerPin;
+  this->slaveSelectPin = _slaveSelect;
+  this->communicationProtocol = _communicationProtocol;
+  this->protocolPin = _protocolPin;
+  
+  if(this->communicationProtocol == TSYS01_SPI)
+  {
+     digitalWrite(this->protocolPin, LOW);
+     pinMode(this->protocolPin, OUTPUT);
+  }
+  if(this->communicationProtocol == TSYS01_I2C)
+  {
+     digitalWrite(this->protocolPin, LOW);
+     pinMode(this->protocolPin, INPUT);
+  }
+  
+  this->reset();
+  delay(4);
+  this->readCalibration();
+}
+
+void Tsys01::readCalibration(void)
+{
+
+  uint8_t coefficient[2];
+  //K0, K1, K2, K3, K4
+  uint8_t coefficientADDR[5] = {  TSYS01_K0ADR,
+                                  TSYS01_K1ADR,
+                                  TSYS01_K2ADR,
+                                  TSYS01_K3ADR,
+                                  TSYS01_K4ADR};
+  
+  for(int iii = 0; iii < 5; iii++)
+  {
+    uint8_t addr = coefficientADDR[iii];
+    uint8_t coefficient[2];
+    this->readBytes(addr, coefficient, 2);
+    this->coefficients[iii] = (uint16_t)((uint16_t)coefficient[0] << 8) + (uint16_t)coefficient[1];
+    #if TSYS_DEBUG
+      Serial.print(iii);
+      Serial.print(". coefficient: ");
+      Serial.println(this->coefficients[iii]);
+    #endif
+  }
+}
+
+void Tsys01::reset()
+{
+  uint8_t payload[0];
+  this->writeBytes(TSYS01_RESET, payload, 0);
+}
+
+void Tsys01::powerOn()
+{
+  pinMode(this->powerPin, INPUT);
+  this->reset();
+}
+
+void Tsys01::powerOff()
+{
+  digitalWrite(this->powerPin, LOW);
+  pinMode(this->powerPin, OUTPUT);
+}
+
+void Tsys01::writeBytes(uint8_t address, uint8_t* bytes, uint8_t numBytes)
+{
+  if(this->communicationProtocol == TSYS01_SPI)
+    return this->spiWriteBytes(address, bytes, numBytes);
+  else 
+    return this->i2cWriteBytes(address, bytes, numBytes);
+}
+
+uint8_t* Tsys01::readBytes(uint8_t address, uint8_t* bytes, uint8_t numBytes)
+{
+  if(this->communicationProtocol == TSYS01_SPI)
+    this->spiReadBytes(address, bytes, numBytes);
+  else 
+    this->i2cReadBytes(address, bytes, numBytes);
+}
+
+uint8_t* Tsys01::i2cReadBytes(uint8_t address, uint8_t* bytes, uint8_t numBytes)
+{
+  //Write Address to read from
+  Wire.begin();
+  Wire.beginTransmission(TSYS01_I2C_ADDR);
+  Wire.write(address);
+  Wire.endTransmission();
+  Wire.end();
+
+  Wire.begin();
+  Wire.requestFrom((uint8_t)TSYS01_I2C_ADDR, numBytes);
+  for(int iii = 0; iii < numBytes && Wire.available(); iii++)
+  {
+    bytes[iii] = Wire.read();
+  }
+  Wire.end();
+  return bytes;
+
+}
+
+void Tsys01::i2cWriteBytes(uint8_t address, uint8_t* bytes, uint8_t numBytes)
+{
+
+  Wire.begin();
+  Wire.beginTransmission(TSYS01_I2C_ADDR); 
+  Wire.write(address);
+  
+  for(int iii = 0; iii < numBytes; iii++)
+  {
+    Wire.write(bytes[iii]);
+  }
+
+  Wire.endTransmission();
+  Wire.end();
+}
+
+void Tsys01::spiReadBytes(uint8_t address, uint8_t* bytes, uint8_t numBytes)
+{
+  //select sensor
+  #if TSYS_DEBUG
+    Serial.println("SPI read bytes");
+  #endif
+  
+  digitalWrite(this->slaveSelectPin, LOW);
+
+  SPI.begin();
+  SPI.beginTransaction(this->spiSettingsObject);
+  SPI.transfer(address);
+  // Read response
+  for(int iii = 0; iii < numBytes; iii++) {
+    bytes[iii] = SPI.transfer(0x00);
+  }
+  SPI.endTransaction();
+  SPI.end();
+  
+  //Deselect sensor
+  digitalWrite(this->slaveSelectPin, HIGH);  
+
+}
+
+void Tsys01::spiWriteBytes(uint8_t address, uint8_t* bytes, uint8_t numBytes)
+{
+  //select sensor
+  digitalWrite(this->slaveSelectPin, LOW);
+  
+  SPI.begin();
+  SPI.beginTransaction(this->spiSettingsObject);
+  SPI.transfer(address);
+
+  for(int iii = 0; iii < numBytes; iii++)
+  {
+    SPI.transfer(bytes[iii]);
+  }
+
+  SPI.endTransaction();
+  SPI.end();
+  
+  //deselect sensor
+  digitalWrite(this->slaveSelectPin, HIGH);
+}
+
+float Tsys01::readTemperature(void)
+{
+  //ADC must have been started first and
+  //10ms must have passed from ADC start
+  //to get a good value.
+  uint8_t rawAdc[3] = {};
+  this->readBytes(TSYS01_READ, rawAdc, 3); 
+  uint16_t ADC16 = ((uint16_t)rawAdc[0]<<8) + (uint16_t)rawAdc[1];
+
+  #if TSYS_DEBUG
+    Serial.print("rawAdc: ");
+    Serial.println((long unsigned int) (((long unsigned int)rawAdc[0] << 16) + ((long unsigned int)rawAdc[1] << 8) + (long unsigned int)rawAdc[2]));
+    Serial.print("ADC16: ");
+    Serial.println(ADC16);
+  #endif
+
+  // calculation of the terms, one by one to avoid accumulating error
+  float term1 = (-2.0) * this->coefficients[4] * pow(TSYS_POW_A * ADC16, 4);
+  float term2 = 4.0    * this->coefficients[3] * pow(TSYS_POW_B * ADC16, 3);
+  float term3 = (-2.0) * this->coefficients[2] * pow(TSYS_POW_C * ADC16, 2);
+  float term4 =          this->coefficients[1] * TSYS_POW_D * ADC16;
+  float term5 = (-1.5) * this->coefficients[0] * TSYS_POW_E;
+
+
+
+  //final temperature
+  float temperature = term1 + term2 + term3 + term4 + term5;
+
+  return temperature;
+}
+
+void Tsys01::startAdc(void)
+{
+  uint8_t temp[0] = {};
+  this->writeBytes(TSYS01_START, temp, 0); //Send start only
+}
---
+++ TSYS01/Arduino/Tsys01.h
@@ -0,0 +1,92 @@
+/*
+	Tsys01.h - library for using ELL-i's Tsys01 breakout.
+	Created by Otso Jousimaa and Jaakko Laine Dec 2015.
+	MIT licensed.
+*/
+#ifndef Tsys01_h
+#define Tsys01_h
+
+#include "Arduino.h"
+#include <math.h> //for calculating powers
+#include <SPI.h>
+
+#define TSYS01_SPI 0
+#define TSYS01_I2C 1
+
+//Change this is you change I2C address through CS pin of board!
+//You can use either read or write address, appropriate bit is set / unset by code
+#define TSYS01_I2C_ADDR (0xEC >> 1) //arduino library uses 7-bit form of address
+//Use Address EE if you pull down CS while using I2C.
+//#define TSYS01_I2C_ADDR (EE >> 1)
+
+#define TSYS01_RESET 0x1E
+#define TSYS01_START 0x48
+#define TSYS01_READ  0x00
+#define TSYS01_ADDR0 0xA0
+#define TSYS01_ADDR1 0xA2
+#define TSYS01_ADDR2 0XA4
+#define TSYS01_ADDR3 0XA6
+#define TSYS01_ADDR4 0XA8
+#define TSYS01_ADDR5 0xAA
+#define TSYS01_ADDR6 0xAC
+#define TSYS01_ADDR7 0xAE
+
+#define TSYS01_K0ADR 0xAA
+#define TSYS01_K1ADR 0xA8
+#define TSYS01_K2ADR 0xA6
+#define TSYS01_K3ADR 0xA4
+#define TSYS01_K4ADR 0xA2
+
+//Made by Apocalyt, precalculated constants of powers of 10
+//This resolves loss of accuracy in calculation of the temperature values resulting from
+const float TSYS_POW_A = 0.0000056234132519034908039495103977648123146825104309869166408; //10^(-21/4)
+const float TSYS_POW_B = 0.0000046415888336127788924100763509194465765513491250112436376; //10^(-16/3)
+const float TSYS_POW_C = 0.0000031622776601683793319988935444327185337195551393252168268; //10^(-11/2)
+const float TSYS_POW_D = 0.000001; // 10^(-6/1)
+const float TSYS_POW_E = 0.01; //10^(-2/1)
+
+
+class Tsys01
+{
+  public: 
+    //constructor
+    Tsys01(uint8_t communicationProtocol, uint8_t powerPin );
+    Tsys01(uint8_t communicationProtocol, uint8_t powerPin, uint8_t slaveSelect);
+    //Use this constructor if software selection of protocol is required, e.g. factory testing.
+    Tsys01(uint8_t communicationProtocol, uint8_t powerPin, uint8_t slaveSelect, uint8_t protocolPin);
+    //This constructor is only for creating global variables.
+    //A constructor with parameters should always be used to get a working Tsys01 object.
+    Tsys01();
+    
+    void startAdc(void);
+    float readTemperature(); //float?
+    //comment about power savings, reboot times
+    void powerOn(); //Remember to check isReady() after power on.
+    void powerOff();
+
+  private:
+    uint16_t coefficients[5];
+    uint8_t communicationProtocol;
+    uint8_t powerPin; 
+    uint8_t slaveSelectPin;
+    uint8_t protocolPin;
+            
+    uint8_t* readBytes(uint8_t address, uint8_t bytes[], uint8_t numBytes);
+    void writeBytes(uint8_t address, uint8_t bytes[], uint8_t numBytes);
+
+    SPISettings spiSettingsObject = SPISettings(1000000UL, MSBFIRST, SPI_MODE0);
+    
+    // Read and write functions for I2C
+    uint8_t* i2cReadBytes(uint8_t address, uint8_t bytes[], uint8_t numBytes);
+    void i2cWriteBytes(uint8_t address, uint8_t bytes[], uint8_t numBytes);
+
+    // Read and write functions for SPI
+    void spiReadBytes(uint8_t address, uint8_t bytes[], uint8_t numBytes);
+    void spiWriteBytes(uint8_t address, uint8_t bytes[], uint8_t numBytes);
+    
+    void readCalibration(void);
+    void reset(void);
+};
+
+#endif
+
